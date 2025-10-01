814cf7cc4c5059d7601ae6ee34c40ee67de1c703
小 小w <116473366+Xiaomichael@users.noreply.github.com>
2025-10-01 05:42:36 -0400

    fix another bug

--- local/Build_SukiSU.sh
+++ local/Build_SukiSU.sh
@@ -142,9 +142,6 @@ if [ "$bbg" = "On" ]; then
     cd ../..
 fi
 
-# --- Kernel Customization ---
-cd kernel_workspace
-
 # Setup SukiSU Ultra
 echo "⚡ Setting up SukiSU Ultra..."
 cd kernel_platform
