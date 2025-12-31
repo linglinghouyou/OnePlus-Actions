8a1fa8490e561f295be57a9397fa71f937311029
小 小w <116473366+Xiaomichael@users.noreply.github.com>
2025-12-31 18:56:35 +0800

    Update Build_Only.sh

--- local/Build_Only.sh
+++ local/Build_Only.sh
@@ -141,7 +141,7 @@ cd kernel_platform
 cp ../kernel_patches/zram/001-lz4.patch ./common/
 cp ../kernel_patches/zram/lz4armv8.S ./common/lib
 cp ../kernel_patches/zram/002-zstd.patch ./common/
-cp ../kernel_patches/common/unicode_bypass_fix_5.10-6.6.patch ./common/
+cp ../kernel_patches/common/unicode_bypass_fix_6.1+.patch ./common/
 
 if [ "$lz4kd" = "On" ]; then
   echo "🚀 正在复制 lz4kd 相关补丁..."
@@ -155,7 +155,7 @@ echo "🔧 正在应用补丁..."
 cd ./common
 
 echo "📦 应用修复Unicode绕过补丁..."
-#patch -p1 < unicode_bypass_fix_5.10-6.6.patch
+patch -p1 < unicode_bypass_fix_6.1+.patch
 
 if [ "$lz4kd" = "Off" ] && [ "$KERNEL_VERSION" = "6.1" ]; then
   echo "📦 正在为 6.1 应用 lz4 + zstd 补丁..."
